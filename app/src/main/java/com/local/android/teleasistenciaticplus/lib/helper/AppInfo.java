e9bc71a4c4111386e72ac3f494da6dc051a1c340
jjfs <gunkanes@gmail.com>
2015-02-13 11:05:22 +0100

    Gitignore Stackoverflow
    http://stackoverflow.com/questions/16736856/what-should-be-in-my-gitignore-for-an-android-studio-project

---
+++ app/src/main/java/com/local/android/teleasistenciaticplus/lib/helper/AppInfo.java
@@ -0,0 +1,15 @@
+package com.local.android.teleasistenciaticplus.lib.helper;
+
+/**
+ * Created by FESEJU on 13/02/2015.
+ * Devolverá valores de configuración de la aplicación como la versión
+ */
+public class AppInfo
+{
+    /**
+     * getAppVersion: devuelve el nombre de la versión recogido en el AndroidManifest
+     */
+    public static String getAppVersion() {
+        return "0.0 prealpha (built 0)";
+    }
+}
