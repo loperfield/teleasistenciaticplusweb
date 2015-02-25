76faaa4b41cde03b6fbdb4b08128671f3d7b8970
GAMO1J <josemanuel.galvez@magtel.es>
2015-02-25 18:30:46 +0100

    Helper para determinar el número de botones a mostrar en AlertDiaplogs

---
+++ app/src/main/java/com/local/android/teleasistenciaticplus/lib/helper/TipoDialogo.java
@@ -0,0 +1,13 @@
+package com.local.android.teleasistenciaticplus.lib.helper;
+
+/**
+ * Created by GAMO1J on 25/02/2015.
+ * Sirve para determinar el número de botones en los Dialogs.
+ * Usada principalmente en AlertDialogShow.java y en todas las clases cuyos métodos requieran
+ * mostrar AlertDialogs
+ */
+public enum TipoDialogo {
+    ONE,
+    TWO,
+    THREE
+}
